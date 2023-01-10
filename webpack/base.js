09a0a345dd1a16cc59c56c1bebd0636e9a0f3da0
dahoon06 <dahoon226@naver.com>
2023-01-10 14:30:19 +0900

    fix: build

--- webpack/base.js
+++ webpack/base.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin({
-            root: path.resolve(__dirname, "game"),
+            root: path.resolve(__dirname, "/"),
         }),
         new webpack.DefinePlugin({
             CANVAS_RENDERER: JSON.stringify(true),
