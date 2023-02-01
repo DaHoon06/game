6576e62d093f5fd2953b164e9e683fed4b7bfcc9
dahoon06 <dahoon226@naver.com>
2023-02-02 00:33:13 +0900

    init: 3번째 게임 시작

--- game3/src/main.ts
+++ game3/src/main.ts
@@ -1,40 +1,4 @@
 import Phaser from 'phaser';
-import {IntroScene} from "./scene/intro.scene";
-import UIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin.js';
+import CONFIG from './config';
 
-const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 1000,
-    height: 1000,
-    parent: 'game',
-    scene: [
-        IntroScene
-    ],
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: true,
-            debugShowBody: true,
-            debugBodyColor: 0xff0000,
-            debugShowStaticBody: true,
-            debugShowVelocity: true,
-            debugStaticBodyColor: 0x0000ff,
-            debugVelocityColor: 0x00ff00,
-        },
-    },
-    dom: {
-        createContainer: true,
-    },
-    plugins: [
-        {
-            key: 'rexUI',
-            plugin: UIPlugin,
-            mapping: 'rexUI'
-        }
-    ],
-    scale: {
-        mode: Phaser.Scale.FIT,
-    },
-};
-export default new Phaser.Game(config);
+export default new Phaser.Game(CONFIG);
--- game3/src/scene/intro.scene.ts
+++ game3/src/scene/intro.scene.ts
@@ -1,15 +1,98 @@
 import {SceneController} from "../controller/scene.controller";
+import * as Phaser from "phaser";
+
 export class IntroScene extends SceneController {
+
     constructor() {
         super('intro');
         console.log('intro');
     }
-    protected async create() {
-    }
+
     protected preload() {
         super.preload();
-        console.log('preload');
+        this.load.spritesheet('brawler', 'character/brawler48x48.png', { frameWidth: 48, frameHeight: 48 });
     }
+
+    protected async create() {
+        this.add.text(200, 312, '<- win', { color: '#00ff00' }).setOrigin(0, 0.5);
+        this.add.image(0, 0, 'brawler', '__BASE').setOrigin(0, 0);
+
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('brawler', { frames: [ 0, 1, 2, 3 ] }),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('brawler', { frames: [ 5, 6, 7, 8 ] }),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'kick',
+            frames: this.anims.generateFrameNumbers('brawler', { frames: [ 10, 11, 12, 13, 10 ] }),
+            frameRate: 8,
+            repeat: -1,
+            repeatDelay: 2000
+        });
+
+        this.anims.create({
+            key: 'punch',
+            frames: this.anims.generateFrameNumbers('brawler', { frames: [ 15, 16, 17, 18, 17, 15 ] }),
+            frameRate: 8,
+            repeat: -1,
+            repeatDelay: 2000
+        });
+
+        this.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNumbers('brawler', { frames: [ 20, 21, 22, 23 ] }),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jumpkick',
+            frames: this.anims.generateFrameNumbers('brawler', { frames: [ 20, 21, 22, 23, 25, 23, 22, 21 ] }),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'win',
+            frames: this.anims.generateFrameNumbers('brawler', { frames: [ 30, 31 ] }),
+            frameRate: 8,
+            repeat: -1,
+            repeatDelay: 2000
+        });
+
+        this.anims.create({
+            key: 'die',
+            frames: this.anims.generateFrameNumbers('brawler', { frames: [ 35, 36, 37 ] }),
+            frameRate: 8,
+        });
+
+        const keys = [ 'walk', 'idle', 'kick', 'punch', 'jump', 'jumpkick', 'win', 'die' ];
+        const cody = this.add.sprite(600, 370, '');
+        cody.setScale(8);
+        cody.play('walk');
+
+        let c = 0;
+        this.input.on('pointerdown', function () {
+            c++;
+            if (c === keys.length)
+            {
+                c = 0;
+            }
+            cody.play(keys[c]);
+        });
+    }
+
+
     update(time: number, delta: number) {
+
     }
 }
