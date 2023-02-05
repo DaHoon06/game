f2d481d15662eb455ef28c3ccbcc6e4b7c10824a
dahoon06 <dahoon226@naver.com>
2023-02-05 11:48:42 +0900

    feat: 상단 메뉴 영역 추가
    TODO
    1. 시간
    2. 킬 카운터

--- game3/src/ui/TopNav.ts
+++ game3/src/ui/TopNav.ts
@@ -3,12 +3,24 @@ import CONFIG from '../config';
 import { SceneController } from "../controller/scene.controller";
 
 export default class TopNav extends Phaser.GameObjects.Graphics {
+  public nav: Phaser.GameObjects.Graphics | null = null;
+
   constructor(scene: SceneController) {
     super(scene);
     const { width } = CONFIG as { width: number}
-    this.fillStyle(0x28288c)
+
+    this.nav = this.fillStyle(0x28288c)
       .fillRect(0, 0, width, 100)
-      .setDepth(999)
       .setScrollFactor(0);
+
+    // this.nav.fillStyle(0x000000);
+    // this.nav.fillRect(this.x, this.y, 80, 16);
+    //
+    // //  Health
+    // this.nav.fillStyle(0xffffff);
+    // this.nav.fillRect(this.x + 2, this.y + 2, 76, 12);
+
+    // 메뉴 그리기
+    this.scene.add.existing(this.nav);
   }
 }
